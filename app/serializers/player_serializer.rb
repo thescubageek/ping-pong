5a70e5e410711817b819518d8f00bbd577be949f
Michael Mitchell <michaelmitchell@gmail.com>
2015-02-07 09:22:35 -0800

    adds player_rating_trend_diff to player serializer

--- app/serializers/player_serializer.rb
+++ app/serializers/player_serializer.rb
@@ -1,5 +1,5 @@
 class PlayerSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :email, :match_wins, :match_losses, :game_wins, 
     :game_losses, :trueskill, :best_buddy_id, :dynamic_duo_id, :ball_and_chain_id, :rival_id,
-    :punching_bag_id, :nemesis_id, :player_rating_trend
+    :punching_bag_id, :nemesis_id, :player_rating_trend, :player_rating_trend_diff
 end
