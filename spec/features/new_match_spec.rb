dfd2da22aefb8e6a324702780409fb042ba704e4
Michael Mitchell <michaelmitchell@gmail.com>
2015-02-27 16:18:35 -0800

    Update new_match_spec.rb

--- spec/features/new_match_spec.rb
+++ spec/features/new_match_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe "New Match" do
-  it 'should be show the link to create a new match' do
+  it 'should show the link to create a new match' do
     visit root_path
     click_link('Start New Match')
     expect(page).to have_content('Team 1')
