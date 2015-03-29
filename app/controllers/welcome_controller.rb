89be726dd17947c526c2d7c5971a10bce789d7a3
Steve Craig <steve.craig@getg5.com>
2015-03-29 10:38:04 -0700

    send new players to new page if not found after sign in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    redirect_to 'players#new' if current_user && !current_player
     @players = Player.by_trueskill
     @matches = Match.includes(:players).limit(10)
   end
