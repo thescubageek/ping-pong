104610834180eeb7575bf25ed9315d935fe4d356
Steve Craig <steve.craig@getg5.com>
2015-04-13 12:33:33 -0700

    update welcome controller redirect

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    redirect_to 'players#new' if current_user && !current_player
+    redirect_to '/players/new' if current_user && !current_player
     @players = Player.no_zeros
     @matches = Match.includes(:players).limit(10)
   end
