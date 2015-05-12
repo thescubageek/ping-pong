3f7756a2249fd928250b4145c398f6b3c8db7836
Steve Craig <steve.craig@getg5.com>
2015-05-12 15:39:55 -0700

    fix weekly rating calc

--- app/models/concerns/player_xp_rating.rb
+++ app/models/concerns/player_xp_rating.rb
@@ -33,7 +33,7 @@ module PlayerXpRating
 
   def has_already_played_opponent_this_week?(match)
     opponent = match.opponent(self)
-    Match.by_player_this_week(self).select { |m| m.opponent(self) == opponent && m != match }.size > 0
+    Match.by_player_and_date_week(self, match.date).select { |m| m.opponent(self) == opponent && m != match }.size > 0
   end
 
 end
